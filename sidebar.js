b1501a1578b84f33b79a82d930d1e38504b583c1
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-02 18:33:05 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,30 +1,46 @@
 miro.onReady(() => {
-  // subscribe on user selected widgets
-  miro.addListener(miro.enums.event.SELECTION_UPDATED, getWidget)
-  getWidget()
-})
 
-// Get html elements for tip and text container
-const tipElement = document.getElementById('tip')
-const widgetTextElement = document.getElementById('widget-text')
+  
+ // The thing
+ var listecartesA = ["https://images.unsplash.com/photo-1591883266273-f10efc5249b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80","https://images.unsplash.com/photo-1591758879097-04f4889fb04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80","https://images.unsplash.com/photo-1591880907925-e8a75dc8cc1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80","https://images.unsplash.com/photo-1591825729269-caeb344f6df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80","https://images.unsplash.com/photo-1588615419966-0c0f3bb797b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"];
 
-async function getWidget() {
-  // Get selected widgets
-  let widgets = await miro.board.selection.get()
-
-  // Get first widget from selected widgets
-  let text = widgets[0].text
-
-  // Check that widget has text field
-  if (typeof text === 'string') {
-
-    // hide tip and show text in sidebar
-    tipElement.style.opacity = '0'
-    widgetTextElement.value = text
-  } else {
-
-    // show tip and clear text in sidebar
-    tipElement.style.opacity = '1'
-    widgetTextElement.value = ''
+ var listecartesB = ["https://images.unsplash.com/photo-1591883266273-f10efc5249b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80","https://images.unsplash.com/photo-1591758879097-04f4889fb04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80","https://images.unsplash.com/photo-1591880907925-e8a75dc8cc1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80","https://images.unsplash.com/photo-1591825729269-caeb344f6df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80","https://images.unsplash.com/photo-1588615419966-0c0f3bb797b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"];
+  
+  var Shuffle = function() {
+  var ctrA = listecartesA.length, temp, index;
+  while (ctrA > 0) {
+  index = Math.floor(Math.random() * ctrA);
+    ctrA--;
+    temp = listecartesA[ctrA];
+    listecartesA[ctrA] = listecartesA[index];
+    listecartesA[index] = temp;
   }
+  var ctrB = listecartesB.length, temp, index;
+  while (ctrB > 0) {
+  index = Math.floor(Math.random() * ctrB);
+    ctrB--;
+    temp = listecartesB[ctrB];
+    listecartesB[ctrB] = listecartesB[index];
+    listecartesB[index] = temp;
+  }
+  return listecartesA, listecartesB;
 }
+
+var getCardA = function() {
+  return listecartesA[0];
+};
+
+var getCardB = function() {
+  return listecartesB[0];
+};
+
+var images = function() {
+ document.getElementById("img1").src=getCardA();
+ document.getElementById("img2").src=getCardB();
+    };
+
+$('#Mélanger').click(function() {
+  Shuffle();
+  images();
+});
+})
