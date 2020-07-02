c6133576c6a9a83a27ebe98b3c0ac9e15291a480
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-02 16:39:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ miro.onReady(() => {
         onClick: async () => {
           
           //Open Sidebar
-          miro.board.ui.openLeftSidebar('sidebar.html')
+          miro.board.ui.openLeftSidebar('https://mimisterio92220.github.io/randomcards/sidebar.html')
 
           // Show success message
           miro.showNotification('Voici les cartes Inspiration & Contexte')
