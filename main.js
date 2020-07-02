69526b572d8d7f6ad61a3cb8867e88fdde43901e
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-02 16:35:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,25 +10,6 @@ miro.onReady(() => {
           //Open Sidebar
           miro.board.ui.openLeftSidebar('sidebar.html')
 
-          // Get selected widgets
-          let selectedWidgets = await miro.board.selection.get()
-
-          // Filter stickers from selected widgets
-          let stickers = selectedWidgets.filter(widget => widget.type === 'STICKER')
-
-          // Delete selected stickers
-          await miro.board.widgets.deleteById(stickers.map(sticker => sticker.id))
-
-          // Create shapes from selected stickers
-          await miro.board.widgets.create(stickers.map(sticker => ({
-            type: 'shape',
-            text: sticker.text,
-            x: sticker.x,
-            y: sticker.y,
-            width: sticker.bounds.width,
-            height: sticker.bounds.height,
-          })))
-
           // Show success message
           miro.showNotification('Voici les cartes Inspiration & Contexte')
         }
