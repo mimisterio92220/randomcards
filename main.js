cbe0a77f658e6e3635cc41db621c27a25f302c2d
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-03 09:25:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ miro.onReady(() => {
     extensionPoints: {
       bottomBar: {
         title: 'Random Cards',
-        svgIcon: 'https://mimisterio92220.github.io/randomcards/Logo.svg',
+        svgIcon: <circle cx=“12” cy=“12" r=“9” fill=“none” fill-rule=“evenodd” stroke=“currentColor” stroke-width=“2”/>,
         positionPriority: 1,
         onClick: async () => {
           
