43b00d8060d7427b5224d8d269c9542f0e3e33c3
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-07 10:39:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 miro.onReady(() => {
+  const icon24 = '<g id="Calque_2" data-name="Calque 2"><g id="Layer_3" data-name="Layer 3"><path class="cls-1" d="M26.82,38,13.74,42.28a.53.53,0,0,1-.67-.34L2.53,10.05a.53.53,0,0,1,.33-.67L23.6,2.53a.52.52,0,0,1,.66.33l3.6,10.89"/><path class="cls-1" d="M31.16,37.81l12.43,4.47a.53.53,0,0,0,.67-.34L54.81,10.05a.53.53,0,0,0-.34-.67L33.74,2.53a.53.53,0,0,0-.67.33l-3.64,11"/><path class="cls-2" d="M28.63,11.74c5.9,7.49,11.09,26.42.24,26.42C16.68,38.16,22.11,19.56,28.63,11.74Z"/></g></g>'
   miro.initialize({
     extensionPoints: {
       bottomBar: {
         title: 'Random Cards',
-        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        svgIcon: icon24,
         positionPriority: 1,
         onClick: async () => {
           
