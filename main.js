4f32dc8fa5f88a8efa39f2a07ee29a5830e2e134
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-20 13:11:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 miro.onReady(() => {
-  const icon24 = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.6 16.03"><defs><style></style></defs><g id="Calque_2" data-name="Calque 2"><g id="Layer_3" data-name="Layer 3"><path class="cls-1" d="M9.63,13.69,4.85,15.27a.19.19,0,0,1-.24-.12L.76,3.5a.19.19,0,0,1,.12-.24L8.45.76a.19.19,0,0,1,.24.12l1.32,4"/><path class="cls-1" d="M11.21,13.64l4.54,1.63a.19.19,0,0,0,.24-.12L19.85,3.5a.2.2,0,0,0-.13-.24L12.15.76a.19.19,0,0,0-.24.12l-1.33,4"/><path class="cls-2" d="M10.29,4.12c2.15,2.74,4.05,9.65.09,9.65C5.93,13.77,7.91,7,10.29,4.12Z"/></g></g></svg>'
+  const icon24 = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.6 16.03"><g id="Calque_2" data-name="Calque 2"><g id="Layer_3" data-name="Layer 3"><path d="M9.63,13.69,4.85,15.27a.19.19,0,0,1-.24-.12L.76,3.5a.19.19,0,0,1,.12-.24L8.45.76a.19.19,0,0,1,.24.12l1.32,4" fill="red" stroke="black" stroke-miterlimit="10" stroke-width="1.5px"/><path d="M11.21,13.64l4.54,1.63a.19.19,0,0,0,.24-.12L19.85,3.5a.2.2,0,0,0-.13-.24L12.15.76a.19.19,0,0,0-.24.12l-1.33,4" fill="red" stroke="black" stroke-miterlimit="10" stroke-width="1.5px"/><path d="M10.29,4.12c2.15,2.74,4.05,9.65.09,9.65C5.93,13.77,7.91,7,10.29,4.12Z" fill="red" stroke="black" stroke-miterlimit="10" stroke-width="1.5px" stroke-linecap="round"/></g></g></svg>'
   miro.initialize({
     extensionPoints: {
       bottomBar: {
