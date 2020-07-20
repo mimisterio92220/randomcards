0e45066258543295492bedc91cd00642ee5ac2a9
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-20 09:36:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 miro.onReady(() => {
-  const icon24test = '<g id="Layer_3"><path class="st0" d="M8.7,11.6l-3.3,1.1c-0.1,0-0.1,0-0.2-0.1c0,0,0,0,0,0L2.5,4.4c0-0.1,0-0.1,0.1-0.2l5.3-1.7c0.1,0,0.1,0,0.2,0.1c0,0,0,0,0,0L9,5.4" fill="none" stroke="#000000" stroke-miterlimit="10"/><path class="st0" d="M9.8,11.5l3.2,1.1c0.1,0,0.1,0,0.2-0.1c0,0,0,0,0,0l2.7-8.1c0-0.1,0-0.1-0.1-0.2c0,0,0,0,0,0l-5.3-1.7c-0.1,0-0.1,0-0.2,0.1L9.4,5.4" fill="none" stroke="#000000" stroke-miterlimit="10"/><path class="st1" d="M9.2,4.9c1.5,1.9,2.8,6.7,0.1,6.7C6.1,11.6,7.5,6.9,9.2,4.9z"fill="none" stroke="#000000" stroke-miterlimit="10" stroke-linecap="round"/></g>'
-  const icon24 = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>'
+  const icon24 = '<g id="Layer_3"><path d=""M9.63,13.69,4.85,15.27a.19.19,0,0,1-.24-.12L.76,3.5a.19.19,0,0,1,.12-.24L8.45.76a.19.19,0,0,1,.24.12l1.32,4" fill="none" stroke="#000000" stroke-miterlimit="10px" stroke-width="1.5px"/></g>'
+  const icon24test = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>'
   miro.initialize({
     extensionPoints: {
       bottomBar: {
