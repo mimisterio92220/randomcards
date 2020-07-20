1e1098c772ff6c985dce26e960fe39fa736799ba
mimisterio92220 <66943796+mimisterio92220@users.noreply.github.com>
2020-07-20 10:52:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 miro.onReady(() => {
-  const icon24 = <g><path d="M10.29,4.12c2.15,2.74,4.05,9.65.09,9.65C5.93,13.77,7.91,7,10.29,4.12Z" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="1.5px" stroke-linecap="round"></g>
-  const icon24test = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>'
+  const icon24 = '<g><path d="M10.29,4.12c2.15,2.74,4.05,9.65.09,9.65C5.93,13.77,7.91,7,10.29,4.12Z" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="1.5px" stroke-linecap="round"></g>'
   miro.initialize({
     extensionPoints: {
       bottomBar: {
